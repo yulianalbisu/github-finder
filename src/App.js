8108f746c3b057f49f417135a31142d570b4988f
Yuliana Aldrich <yuliana.aldrich@perficient.com>
2022-05-11 06:43:47 -0400

    adding alert and working app as expected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/layout/Users';
 import Search from './components/layout/Search';
+import Alert  from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,7 +10,8 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // async componentDidMount() {
@@ -19,26 +21,35 @@ class App extends Component {
 // search github users
   searchUsers = async text => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-    &client_secret=client_id=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}
+    &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
     this.setState({ users: res.data.items, loading: false });
   }
 
   clearUsers =() => this.setState({ users: [], loading:false });
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }})
+  }
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar />
         <div className='container'>
+          <Alert alert={ this.state.alert } />
           <Search searchUsers={this.searchUsers} 
           clearUsers={this.clearUsers} 
-          showClear={ this.state.users.length > 0 ? true : false }>
+          showClear={ users.length > 0 ? true : false }
+          setAlert={this.setAlert}
+          />
           
-        <Users loading={this.state.loading} 
-        users={this.state.users} />   
-        </Search>
+        <Users loading={ loading} 
+        users={ users } />   
         </div>
            
   </div>
